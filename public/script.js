8d3d6dc20a4f6fbd5762ad532dc6e7dbdbaab487
vickysawant19 <vicky.sawant19@gmail.com>
2024-11-13 08:46:13 +0530

    update 0001

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
-// const socket = io("https://bingo-vs.up.railway.app");
-const socket = io("http://localhost:3000");
+const socket = io("https://bingo-vs.up.railway.app");
+// const socket = io("http://localhost:3000");
 
 socket.on("connect", () => {
   console.log("Connected to server");
